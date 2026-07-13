39faf2a1c083ff15406d1f28a39d987741be405f
Vivek Agrawal <vivek@incubyte.co>
2026-07-13 18:06:43 +0530

    feat(openapi): exposed Swagger for API endpoints

--- task-management/src/main.ts
+++ task-management/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const HOST = process.env.HOST ?? '0.0.0.0';
@@ -6,6 +7,15 @@ const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(PORT, HOST);
 }
 
