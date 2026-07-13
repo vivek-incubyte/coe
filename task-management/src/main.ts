2d1923f2a06eaee44dbb210f51ab7575cee7474c
Vivek Agrawal <vivek@incubyte.co>
2026-07-13 16:20:58 +0530

    fix: exposed 0.0.0.0 instead of localhost

--- task-management/src/main.ts
+++ task-management/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const HOST = process.env.HOST ?? '0.0.0.0';
+const PORT = process.env.PORT ?? 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT, HOST);
 }
 
 bootstrap().catch((err) => {
